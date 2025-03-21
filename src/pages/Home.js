a8bddc8a1bc0e1256ff8dde95777752606a42a40
seito <arasei1019@gmail.com>
2025-03-21 19:12:14 +0900

    useState 初期値を変更

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 
 export const Home = () => {
-  const [posts, setPosts] = useState([null]);
+  const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
 
 
